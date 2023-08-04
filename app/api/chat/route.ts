fa486f0e68e461f75794b0794b273d5d9c064db6
duhfrazee <54338114+duhfrazee@users.noreply.github.com>
2023-08-04 09:24:51 -0500

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,11 +1,23 @@
-import { StreamingTextResponse } from 'ai'
+import { kv } from '@vercel/kv'
+import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { Configuration, OpenAIApi } from 'openai-edge'
 
 import { auth } from '@/auth'
+import { nanoid } from '@/lib/utils'
+
+export const runtime = 'edge'
+
+const configuration = new Configuration({
+  //apiKey: 'ASStn8eNtkg87W5wemGhcMfOPxJ8/hyVaabGZ0tmsj8=' local version
+  apiKey: process.env.TENNR_API_KEY
+})
+
+const openai = new OpenAIApi(configuration)
 
 export async function POST(req: Request) {
   try {
     const json = await req.json()
-    const { messages } = json
+    const { messages, previewToken } = json
     const userId = (await auth())?.user.id
 
     if (!userId) {
@@ -14,16 +26,23 @@ export async function POST(req: Request) {
       })
     }
 
-    const agentId = process.env.TENNR_AGENT_ID;
+    if (previewToken) {
+      configuration.apiKey = previewToken
+    }
+
+    // Authorization: `api-key ASStn8eNtkg87W5wemGhcMfOPxJ8/hyVaabGZ0tmsj8=` local version
+    //const agentId = '64c5709065736e592c4b67cf' local version
+    const agentId = '644b3fbae8f5dfe23bd06482'
+    //const agentUrl = 'http://localhost:4000' local version
     const agentUrl = 'https://agent.tennr.com'
-    var streamIt = false
+    var streamIt = true
 
     const response = await fetch(agentUrl + '/api/v1/workflow/run', {
       method: 'POST',
       mode: 'cors',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `api-key ${process.env.TENNR_API_KEY}`
+        Authorization: `api-key ${process.env.TENNR_API_KEY}` //gv8pzQgsLyTInP4mO02VSkMdA0zxsL97iRkAMw33NRU=`
       },
       body: JSON.stringify({
         agentId: agentId,
@@ -49,11 +68,14 @@ export async function POST(req: Request) {
         .replace(/{"sources":\[.*\]}/, '')
         .trim()
 
-      responseStream = arrayToStream([cleanedMessages])
+      const processedMessages = createWordArray(cleanedMessages)
+      responseStream = arrayToStream(processedMessages)
+      console.log('final product for streaming: ', processedMessages)
     } else {
       const responseJson = JSON.parse(await response.text())
       const outputText = responseJson.output // Extract the output text.
       responseStream = stringToStream(outputText)
+      console.log('Final product for non-streaming: ', outputText)
     }
 
     return new StreamingTextResponse(responseStream)
@@ -86,4 +108,9 @@ function arrayToStream(array: string[]) {
       }
     }
   })
+}
+
+function createWordArray(text: string): string[] {
+  const wordArray = text.split(' ')
+  return wordArray
 }
